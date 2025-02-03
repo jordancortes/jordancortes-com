b884e64b6bf701a150f08e81ddeaa933e23997a2
Jordan Cortes <hello@jordancortes.com>
2025-02-02 23:47:23 -0600

    add logo as landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="@container">
+  <div class="flex h-screen place-content-center mx-32">
+    <img style="filter: invert(91%) sepia(8%) saturate(697%) hue-rotate(337deg) brightness(105%) contrast(85%);"  alt="" src="logo.svg" />
+  </div>
+</div>
