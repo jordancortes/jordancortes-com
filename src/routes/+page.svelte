860e6b8ffb25098b0e0c9b392023eb09855e0345
Jordan Cortes <hello@jordancortes.com>
2025-02-08 15:31:40 -0600

    add new favicon and page title

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,7 @@
+<svelte:head>
+  <title>Jordan Cortes - Full Stack Developer</title>
+</svelte:head>
+
 <div class="@container">
   <div class="flex h-screen place-content-center mx-32">
     <img style="filter: invert(91%) sepia(8%) saturate(697%) hue-rotate(337deg) brightness(105%) contrast(85%);"  alt="" src="logo.svg" />
